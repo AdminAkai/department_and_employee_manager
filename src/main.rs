84ccb869319e476d2f3a9028ecd02df1813c9fed
AdminAkai <akatoirotech@gmail.com>
2024-07-18 03:02:03 -0400

    department and employee manager

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::collections::HashMap;
+
+fn main() {
+    let mut run = true;
+    let mut department_data: HashMap<String, Vec<String>> = HashMap::new();
+    
+    while run {    
+        let mut department: String = String::new();
+        let mut employee: String = String::new();
+        let mut end_input: String = String::new();
+
+        println!("Which department are you adding an employee to?");
+        io::stdin().read_line(&mut department).expect("Failed to take input.");
+        department = String::from(department.trim());
+
+        println!("The {} department. What is the employee's name?", &department);
+        io::stdin().read_line(&mut employee).expect("Failed to take input.");
+        employee = String::from(employee.trim());
+
+        department_data.entry(department).and_modify(|e| { e.push(employee.clone()) }).or_insert(vec![employee]);
+        
+        println!("Current departments:");
+        for (key, val) in &department_data {
+            println!("{key}:");
+            println!("{val:?}");
+        }
+
+        println!("Press any key to continue or \"quit\" to quit.");
+        io::stdin().read_line(&mut end_input).expect("Failed to take input.");
+        end_input = String::from(end_input.trim());
+
+        if end_input == "quit" {
+            run = false;
+        }
+    }
+}
+
+
